4a5de95404a6df0ed066571f4641c1537aa3cc56
Don Yu <devzons@gmail.com>
2019-11-02 03:04:35 -0700

    Route parameters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends React.Component {
 	render() {
@@ -13,7 +14,8 @@ class App extends React.Component {
 					<Navbar />
 					<Route exact path='/' component={Home} />
 					<Route path='/about' component={About} />
-					<Route path='/contact' component={Contact} />
+					<Route exact path='/contact' component={Contact} />
+					<Route path='/:post_id' component={Post} />
 				</div>
 			</BrowserRouter>
 		)
