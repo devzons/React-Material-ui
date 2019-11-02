2dd17196606b16186d8dbd27dfb3772f3be874b0
Don Yu <devzons@gmail.com>
2019-11-02 02:39:33 -0700

    get external data using axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,40 @@
 import React from 'react'
+import axios from 'axios'
 
-const Home = () => {
-	return (
-		<div className='container'>
-			<h4 className='center'>Home</h4>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim sed animi
-				inventore omnis, facere eos? Earum fugit harum explicabo ut ducimus
-				possimus distinctio officia ipsum. Veritatis, aspernatur odio. Itaque,
-				culpa.
-			</p>
-		</div>
-	)
+class Home extends React.Component {
+	state = {
+		posts: []
+	}
+	componentDidMount() {
+		axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+			this.setState({
+				posts: res.data.slice(0, 10)
+			})
+		})
+	}
+	render() {
+		const { posts } = this.state
+		const postList = posts.length ? (
+			posts.map(post => {
+				return (
+					<div className='post card' key={post.id}>
+						<div className='card-content'>
+							<span className='card-title'>{post.title}</span>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				)
+			})
+		) : (
+			<div className='center'>No Posts Yet</div>
+		)
+		return (
+			<div className='container'>
+				<h4 className='center'>Home</h4>
+				{postList}
+			</div>
+		)
+	}
 }
 
 export default Home
